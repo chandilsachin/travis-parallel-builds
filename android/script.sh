73076675ccb3700f1b4297b17018fe7b619cb39f
Sachin Chandil <chandilsachin@gmail.com>
2018-09-13 00:16:52 +0530

    Initial commit

---
+++ android/script.sh
@@ -0,0 +1,43 @@
+function before_cache {
+    return 0;
+}
+
+function before_install {
+    return 0;
+}
+
+function install {
+    sleep 5s
+    return 0;
+}
+
+function before_script {
+    return 0;
+}
+
+function script_test {
+    sleep 5s
+    echo "All test cases passed successfully."
+    return 0;
+}
+
+function script_build {
+    sleep 1m
+    echo "Build finished."
+    return 0;
+}
+
+function before_deploy {
+    return 0;
+}
+
+function deploy {
+    echo "Deployed successfully."
+    return 0;
+}
+
+function after_deploy {
+    return 0;
+}
+
+$1 "$@"
